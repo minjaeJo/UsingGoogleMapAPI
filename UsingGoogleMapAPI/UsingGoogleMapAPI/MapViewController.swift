afdeb4495c9a64c3ccb2a7be189d8acce854e002
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-04-25 19:57:33 +0900

    카페정보 불러오기 및 함수 분할

--- UsingGoogleMapAPI/UsingGoogleMapAPI/MapViewController.swift
+++ UsingGoogleMapAPI/UsingGoogleMapAPI/MapViewController.swift
@@ -15,12 +15,14 @@ import SwiftyJSON
 class MapViewController: UIViewController, GMSMapViewDelegate,CLLocationManagerDelegate {
 
     @IBOutlet weak var googleMap: GMSMapView!
-    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var cafeAddress: UILabel!
+    @IBOutlet weak var cafeName: UILabel!
+    @IBOutlet weak var cafePicture: UIImageView!
     
     var locationManager = CLLocationManager()
     var currentLocation: CLLocation?
     var placesClient: GMSPlacesClient!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //locationManager - 좌표 알려주는 매니져
@@ -48,67 +50,72 @@ class MapViewController: UIViewController, GMSMapViewDelegate,CLLocationManagerD
         marker.icon = iconMarker
         marker.map = googleMap
     }
-    
-    // Part - Delegate : Location Manager Delegate
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error to get lotation \(error)")
-    }
-    
-    
-    
-// Part - Delegate : GMSMapViewDelegate
+
     func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
         googleMap.isMyLocationEnabled = true
     }
     
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
-        convertToAddress(latitude: coordinate.latitude, longtitude: coordinate.longitude) { (address) in
-            self.locationLabel.text = address
-            print(address)
-        }
-        around()
+        googleMap.clear()
+
+//        searchCafeAroundMe(latitude: coordinate.latitude, longtitude: coordinate.longitude) { (cafeInfoArray) in
+//                spreadMarker(cafeInfo: cafeInfoArray)
+//        }
     }
-    
-//    func didTapMyLocationButton(for mapView: GMSMapView) -> Bool {
-//
-//    }
-    func convertToAddress(latitude : CLLocationDegrees, longtitude: CLLocationDegrees, completion : @escaping (String) -> Void) {
-        let url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=\(latitude),\(longtitude)"
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        print("yo~")
+        return true
+    }
+
+
+    // 내 주위 700미터 안의 카페 탐색
+    func searchCafeAroundMe (latitude : CLLocationDegrees, longtitude: CLLocationDegrees, completion : @escaping ([[String : Any]]) -> Void) {
         
-        Alamofire.request(url).responseJSON{ responds in
-            print(responds.request as Any)
-            print(responds.response as Any)
-            print(responds.data as Any)
-            print(responds.result as Any)
-            
-            let json = JSON(data: responds.data!)
-            let result = json["results"].arrayValue
-            let premiseTypeAddress = result[0].dictionary
-            let formattedAddress = (premiseTypeAddress?["formatted_address"]?.stringValue)!
-            completion(formattedAddress)
-        }
-    }
-    func around () {
-        let url = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=37.404796,127.106053&radius=3000&type=cafe&key=AIzaSyD2McX3Ev3I5C-ZT-l8EsbVO9YMFcsjfcQ"
-        Alamofire.request(url).responseJSON { responds in
-            print(responds.request as Any)
-            print(responds.response as Any)
-            print(responds.data as Any)
-            print(responds.result as Any)
+        var cafeInfo = [String : Any]()
+        var cafeInfoArray = [[String : Any]]()
+        
+        let aroundMeURL = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=\(latitude),\(longtitude)&radius=700&type=cafe&key=AIzaSyD2McX3Ev3I5C-ZT-l8EsbVO9YMFcsjfcQ"
+    
+        Alamofire.request(aroundMeURL).responseJSON { responds in
             let json = JSON(data: responds.data!)
             let results = json["results"].arrayValue
+            
             for result in results {
                 let geometry = result["geometry"].dictionaryValue
                 let location = geometry["location"]?.dictionary
-                let locationLatitude = location?["lat"]?.doubleValue
-                let locationLongtitude = location?["lng"]?.doubleValue
-                //let place_id = result[""]
-//
-                self.createMarker(titleMarker: "카페", iconMarker: #imageLiteral(resourceName: "startPin") , latitude: locationLatitude!, longitude: locationLongtitude!)
-//                
+                let cafeLatitude = location?["lat"]?.doubleValue
+                let cafeLongtitude = location?["lng"]?.doubleValue
+                let cafeInfoID = result["place_id"].stringValue
+                
+                cafeInfo["cafeLatitude"] = cafeLatitude
+                cafeInfo["cafeLongtitude"] = cafeLongtitude
+                cafeInfo["cafeInfoID"] = cafeInfoID
+                cafeInfoArray.append(cafeInfo)
             }
+        }
+        completion(cafeInfoArray)
+    }
+    
+    func spreadMarker (cafeInfo : [[String : Any]]) {
+        
+        for  in cafeInfo {
             
         }
     }
+    //self.createMarker(titleMarker: "카페", iconMarker: #imageLiteral(resourceName: "startPin") , latitude: cafeLatitude!, longitude: cafeLongtitude!)
+    //            let cafeURL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJoxjnPvSnfDURgtAkn8HmE_8&language=ko&key=AIzaSyD2McX3Ev3I5C-ZT-l8EsbVO9YMFcsjfcQ"
+    //
+    //            Alamofire.request(cafeURL).responseJSON { responds in
+    //                let json = JSON(data: responds.data!)
+    //                let result = json["result"].dictionary
+    //                let address = result?["formatted_address"]?.stringValue
+    //                let name = result?["name"]?.stringValue
+    //
+    //                self.cafeAddress.text = address
+    //                self.cafeName.text = name
+    //
+    //            }
 }
+
+
 
